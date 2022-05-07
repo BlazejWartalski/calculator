18701bae7ca5356d5f67dd0a346a6c7ae1c62972
Blazej <BlazejWartalski97@gmail.com>
2022-05-07 23:43:02 +0100

    complete calculator

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+let display = document.querySelector('.screen');
+
+let displayNumber = "";
+let a = null;
+let b = "";
+display.innerHTML = displayNumber;
+
+
+function printNumber(e) {
+    displayNumber = e.target.innerHTML;
+    previous = display.innerHTML;
+
+    if (display.innerHTML.includes(".") && displayNumber.includes(".")){
+        return;
+    }
+    display.innerHTML = previous+displayNumber;
+}
+const btns = document.querySelectorAll('.number');
+for (var i = 0 ; i < btns.length; i++) {
+    btns[i].addEventListener('click',printNumber ) ; 
+}
+
+const equalBtn = document.querySelector('.equalbtn')
+equalBtn.addEventListener('click', calculate) ;
+
+const backspaceBtn = document.querySelector('.backspace')
+backspaceBtn.addEventListener('click', backspace) ;
+
+const clearBtn = document.querySelector('.clear')
+clearBtn.addEventListener('click', clear) ;
+
+function backspace(){
+    display.innerHTML = display.innerHTML.slice(0, -1);
+};
+
+function clear(){
+    display.innerHTML = "";
+    a = "";
+    b= "";
+};
+
 function add (a, b) {
     return a + b;
 };
@@ -5,11 +46,6 @@ function add (a, b) {
 function subtract(a, b) {
     return a - b;
 };
-  
-//   const sum = function(array) {
-//       const sum = array.reduce((a, b) => a + b, 0);
-//     return sum
-//   };
   
 function multiply(a, b) {
     return a * b;
@@ -23,23 +59,63 @@ const power = function(a,b) {
     const power = a ** b;
     return power
 };
-console.log(subtract(1,3))
-const operate = function(operate, a, b) {
-    if (operate == "add") {
-        console.log("fuckj")
-        return add(a, b);
-    }
-    if (operate == "subtract") {
-        console.log(subtract (a,b));
-        return subtract(a, b);
-    }
-    if (operate == "multiply") {
-        return multiply(a,b);
-    }
-    if (operate == "divide") {
-        return divide(a,b);
-    }
+
+function operate(e) {
+    operant = (e.target.innerHTML);
+    a = (display.innerHTML);
+    display.innerHTML = '';
 }
+
+function calculate() {
+    if (a != null) {
+        b = (display.innerHTML);
+        c = parseInt(a + operant + b);
+    }
+    if (operant == '-') {
+        display.innerHTML = Math.round((a - b)* 1000)/1000;
+        a = b;
+    }
+    if (operant == '+') {
+        console.log(+a + +b);
+        display.innerHTML = Math.round((+a + +b)* 1000)/1000;
+        a = b;
+    }
+    if (operant == '*') {
+        display.innerHTML = Math.round((a * b)* 1000)/1000;
+        a = b;
+    }
+    if (operant == '/') {
+        if (b == 0) {
+            alert("can't divide by 0")
+            console.log(a);
+            console.log(b);
+            return
+        }
+        display.innerHTML = Math.round((a / b) * 1000)/1000;
+        a = b;
+    }
+};
+// const operate = function(operate, a, b) {
+//     if (operate == "add") {
+//         console.log("fuckj")
+//         return add(a, b);
+//     }
+//     if (operate == "subtract") {
+//         console.log(subtract (a,b));
+//         return subtract(a, b);
+//     }
+//     if (operate == "multiply") {
+//         return multiply(a,b);
+//     }
+//     if (operate == "divide") {
+//         return divide(a,b);
+//     }
+// }
+
+const operants = document.querySelectorAll('.operator');
+for (var i = 0 ; i < operants.length; i++) {
+    operants[i].addEventListener('click',operate ) ; 
+};
 
 //   const factorial = function(n) {
 //       if (n === 0) return 1;
@@ -49,3 +125,12 @@ const operate = function(operate, a, b) {
 //     }
 //     return product;
 //   };
+
+
+// PLAN
+// Numbers in display 
+// When press any operators, save the current display as a number in string
+// Clear the display
+// Enter more numbers
+// On equal sign press, return the equation result in the display.
+//
